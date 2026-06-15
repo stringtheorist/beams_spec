fixed bug with numerical computation ; improved graphs.

beams_spec/beams_spec_core.py
@@ -567,7 +567,7 @@ def compute_discrete_k2(s_grid, t_grid, sparams:SolutionParameters):
     k2_numeric = np.zeros((ns, nt), dtype=complex)
 
     for ii in range(ns):
-        for jj in range(ns):
+        for jj in range(nt):
             k2_numeric[ii, jj] = k2(s_grid[ii], t_grid[jj], sparams)
 
     return k2_numeric
@@ -580,7 +580,7 @@ def compute_discrete_e1(s_grid, t_grid, sparams:SolutionParameters):
     e1_numeric = np.zeros((ns, nt), dtype=complex)
 
     for ii in range(ns):
-        for jj in range(ns):
+        for jj in range(nt):
             e1_numeric[ii, jj] = e1(s_grid[ii], t_grid[jj], sparams)
 
     return e1_numeric
@@ -593,7 +593,7 @@ def compute_discrete_v1(s_grid, t_grid, sparams:SolutionParameters):
     v1_numeric = np.zeros((ns, nt), dtype=complex)
 
     for ii in range(ns):
-        for jj in range(ns):
+        for jj in range(nt):
             v1_numeric[ii, jj] = v1(s_grid[ii], t_grid[jj], sparams)
 
     return v1_numeric
@@ -606,10 +606,25 @@ def compute_discrete_o2(s_grid, t_grid, sparams:SolutionParameters):
     o2_numeric = np.zeros((ns, nt), dtype=complex)
 
     for ii in range(ns):
-        for jj in range(ns):
+        for jj in range(nt):
             o2_numeric[ii, jj] = o2(s_grid[ii], t_grid[jj], sparams)
 
     return o2_numeric
+
+def compute_discrete_theta(s_grid, t_grid, sparams:SolutionParameters):
+
+    nt = np.size(t_grid)
+    ns = np.size(s_grid)
+
+    theta_numeric = np.zeros((ns, nt), dtype=complex)
+
+    for ii in range(ns):
+        for jj in range(nt):
+            theta_numeric[ii, jj] = theta(s_grid[ii], t_grid[jj], sparams)
+
+    return theta_numeric
+
+
 
 
 # def compute_phi_via_bcs(s_grid, t_grid, e1_numeric, k2_numeric, sparams:SolutionParameters):

beams_spec/beams_spec_visualisation.py
@@ -84,12 +84,12 @@ def visually_inspect_efuncs(efparams:BasisParameters, norms_ef, s, wc):
     plt.show()
 
 
-def simulate_beam(phi1, phi3, s, t, sparams:SolutionParameters):
+def simulate_beam_exact_theta(phi1, phi3, s_grid, t_grid, sparams:SolutionParameters, save=False):
     """animate the beam motion"""
     
     ns = np.shape(phi1)[0]
     nt = np.shape(phi1)[1]
-    s = s.reshape((ns,))
+    s_grid = s_grid.reshape((ns,))
 
     fig, ax = plt.subplots()
 
@@ -100,12 +100,16 @@ def simulate_beam(phi1, phi3, s, t, sparams:SolutionParameters):
 
     for ii in range(ns):
         for jj in range(nt):
-            sines[ii, jj] = np.sin(theta(s[ii], t[jj], sparams))
-            cosines[ii, jj] = np.cos(theta(s[ii], t[jj], sparams))
+            sines[ii, jj] = np.sin(theta(s_grid[ii], t_grid[jj], sparams))
+            cosines[ii, jj] = np.cos(theta(s_grid[ii], t_grid[jj], sparams))
 
     def update(it):
 
         ax.clear()
+        text_box_props = dict(boxstyle='round', alpha=0.5)
+        textstr = f"t = {t_grid[it]:0.2f}" 
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+                verticalalignment='top', bbox=text_box_props)
        
         for i in range(ns):
             phx_s[i] = -phi1[i, it]*sines[i, it] + phi3[i, it]*cosines[i, it]
@@ -113,9 +117,9 @@ def simulate_beam(phi1, phi3, s, t, sparams:SolutionParameters):
 
         medial = ax.plot(phx_s, phy_s)
 
-        q1 = ax.quiver(phx_s, phy_s, -sines[:, it], cosines[:, it])
-        q2 = ax.quiver(phx_s, phy_s, sines[:, it], -cosines[:, it])
-        ax.set(xlim=[0.0, 60.0], ylim=[-50.0, +50.0])
+        q1 = ax.quiver(phx_s, phy_s, -sines[:, it], cosines[:, it], headaxislength=0, headlength=0)
+        q2 = ax.quiver(phx_s, phy_s, sines[:, it], -cosines[:, it], headaxislength=0, headlength=0)
+        ax.set(xlim=[0.0, 60.0], ylim=[-60.0, +60.0])
         ax.set_aspect('equal', adjustable='box')
         
         sleep(0.1)
@@ -123,10 +127,13 @@ def simulate_beam(phi1, phi3, s, t, sparams:SolutionParameters):
         return medial
 
     anim = FuncAnimation(fig=fig, func=update, frames=nt)
-    plt.show()
+    if save==True:
+        anim.save(filename="beam_sim_exact_theta.mp4", writer="ffmpeg")
+    else:
+        plt.show()
     
-        
-def simulate_beam_approx_theta(phi1, phi3, cos_theta, sin_theta, s_grid, t_grid):
+      
+def simulate_beam_approx_theta(phi1, phi3, cos_theta, sin_theta, s_grid, t_grid, save=False):
     """Uses externally computed theta"""
     ns = np.shape(phi1)[0]
     nt = np.shape(phi1)[1]
@@ -141,6 +148,11 @@ def simulate_beam_approx_theta(phi1, phi3, cos_theta, sin_theta, s_grid, t_grid)
     def update(it):
 
         ax.clear()
+        text_box_props = dict(boxstyle='round', alpha=0.5)
+        textstr = f"t = {t_grid[it]:0.2f}" 
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+                verticalalignment='top', bbox=text_box_props)
+
        
         for i in range(ns):
             phx_s[i] = -phi1[i, it]*sin_theta[i, it] + phi3[i, it]*cos_theta[i, it]
@@ -148,21 +160,24 @@ def simulate_beam_approx_theta(phi1, phi3, cos_theta, sin_theta, s_grid, t_grid)
 
         medial = ax.plot(phx_s, phy_s)
 
-        q1 = ax.quiver(phx_s, phy_s, -sin_theta[:, it], cos_theta[:, it])
-        q2 = ax.quiver(phx_s, phy_s, sin_theta[:, it], -cos_theta[:, it])
+        q1 = ax.quiver(phx_s, phy_s, -sin_theta[:, it], cos_theta[:, it], headaxislength=0, headlength=0)
+        q2 = ax.quiver(phx_s, phy_s, sin_theta[:, it], -cos_theta[:, it], headaxislength=0, headlength=0)
         ax.set(xlim=[0.0, 60.0], ylim=[-60.0, +60.0])
         ax.set_aspect('equal', adjustable='box')
         
         sleep(0.1)
         fig.canvas.draw()
         return medial
-
+    
     anim = FuncAnimation(fig=fig, func=update, frames=nt)
-    plt.show()
+    if save == True: 
+        anim.save(filename="beam_sim_approx_theta.mp4", writer="ffmpeg")
+    else:
+        plt.show()
 
-    
 
-    
+
+  
     
     
     

test_3.py
@@ -1,9 +1,9 @@
-# *Global imports*
-import os
-nthreads = 8
-os.environ["OMP_NUM_THREADS"] = str(nthreads) 
-os.environ["OPENBLAS_NUM_THREADS"] = str(nthreads) 
-os.environ["MKL_NUM_THREADS"] = str(nthreads)
+# # *Global imports*
+# import os
+# nthreads = 8
+# os.environ["OMP_NUM_THREADS"] = str(nthreads) 
+# os.environ["OPENBLAS_NUM_THREADS"] = str(nthreads) 
+# os.environ["MKL_NUM_THREADS"] = str(nthreads)
 
 
 import numpy as np
@@ -11,12 +11,17 @@ import scipy as sp
 from numpy.lib.scimath import sqrt
 from scipy.io import loadmat
 from numpy.linalg import norm
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
 
 # *Local imports*
 from beams_spec.beams_spec_core import *
 from beams_spec.beams_spec_visualisation import *
 
-
+# TODO: timing data for various steps
+# TODO: experimentation integration in time.
+# TODO: investigation of instabilities - integrals.
+# TODO: 
 #Nondimensional phyisical parameters
 L = 50.0
 g = 2.5
@@ -68,9 +73,61 @@ phi1_numeric, phi3_numeric = compute_phi_from_e1_k2(s_grid, t_grid,
 
 cos_theta, sin_theta = compute_theta_from_k(s_grid, t_grid, k2_numeric)
 
+theta_exact = compute_discrete_theta(s_grid, t_grid, solution_params)
+
+cos_theta_exact = np.cos(theta_exact)
+sin_theta_exact = np.sin(theta_exact)
+
+e_cos = norm(cos_theta - cos_theta_exact)/norm(cos_theta_exact)
+e_sin = norm(sin_theta - sin_theta_exact)/norm(sin_theta_exact)
+
+print(f'Relative error cos(theta) between exact and approx = {e_cos:1.5e}')
+print(f'Relative error sin(theta) between exact and approx = {e_sin:1.5e}')
+
 #Visualise the solution
-#simulate_beam(phi1_numeric, phi3_numeric, s_grid, t_grid, solution_params)
-simulate_beam_approx_theta(phi1_numeric, phi3_numeric, cos_theta, sin_theta, s_grid, t_grid)
+# simulate_beam_exact_theta(phi1_numeric, phi3_numeric, s_grid, t_grid, solution_params, save=True)
+# simulate_beam_approx_theta(phi1_numeric, phi3_numeric, cos_theta, sin_theta, s_grid, t_grid, save=True)
+
+# e1 et k2 au cours du temps. a un point pres de 3*L/4
+
+# inspection_point = (3.0*L)/4.0
+
+# dists = np.abs(s_grid - inspection_point)
+# inspection_point_id = np.argmin(dists)
+
+# print(f'Grid inspection point:{inspection_point_id}')
+
+# fig_inspection, axes = plt.subplots(2, 1)
+
+
+# t_plot_grid = np.linspace(0.0, t_max, 10000)
+
+# k2_t = np.zeros(np.shape(t_plot_grid))
+# e1_t = np.zeros(np.shape(t_plot_grid))
+
+# for t_i in range(np.size(t_plot_grid)):
+#     k2_t[t_i] = k2(inspection_point, t_plot_grid[t_i], solution_params)
+#     e1_t[t_i] = e1(inspection_point, t_plot_grid[t_i], solution_params)
+    
+
+
+# axes[0].plot(t_plot_grid, k2_t)
+# axes[1].plot(t_plot_grid, e1_t)
+# fig_inspection.tight_layout()
+# plt.show()
+
+# s_ = rc * s et t_ = tc * t
+# rc = r/sqrt(2)
+# L_ = rc * L
+# tc = rc * sqrt(rho/E) ; rho ~ 4000 ; E ~ 200 * 10^9
+# r = 5 * 10^(-2)
+# L_ = 15 m
+# L = L_/(r / sqrt(2))
+# A =  pi * r^2
+# I3 = 2 (pi * r^4)
+
+
+
 
 
 